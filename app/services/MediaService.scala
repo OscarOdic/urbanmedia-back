1c061cc210e9dbe7239253a3c07b8033ce1e330e
Oscar Odic <odic.oscar@gmail.com>
2017-12-28 18:04:22 +0100

    Delete image/song/video

--- app/services/MediaService.scala
+++ app/services/MediaService.scala
@@ -13,6 +13,7 @@ object MediaService {
   def getMediasFromPlace[T <: Medias](table: TableQuery[T], tableName: String)(placeId: Int)(db: DatabaseDef): Future[List[Media]] =
     db.run(table.filter(_.placeId === placeId).map(media => (media.author, media.id)).result).map(_.toList.map {
       case (author, mediaId) => Media(
+        Some(mediaId),
         author,
         s"${ConfigFactory.load().getString("application.baseUrl")}place/$tableName?placeid=$placeId&${tableName}id=$mediaId"
       )
